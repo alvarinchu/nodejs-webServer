3f6f9d4d12a7866882d10bd50a867f4b8180c9a2
Alvaro <alvarinchu@gmail.com>
2021-01-08 19:40:55 +0100

    commit3

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express')
-const app = express()
- 
-app.get('/data/:id/:name', function (req, res) {
+const { static } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+app.get('/data/:id/:name?', function (req, res) {
     console.log(req.params);
     console.log(req.query);
-    res.send(`Hello World ${req.url}`)
-})
+    res.send(`Hello World ${req.url}`);
+});
 
 app.post('/data', (req, res) => {
     console.log(req.body);
     console.log(req.params);
     console.log(req.query);
-    res.send({ nombre: 'Álvaro', apellidos: 'Pérez'})
+    res.send({ nombre: 'Álvaro', apellidos: 'Pérez Rodrigo' });
 });
- 
+
 app.listen(3000, () => {
     console.log('Escuchando en puerto 3000');
-})
+});
