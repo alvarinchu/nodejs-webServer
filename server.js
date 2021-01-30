211b7951468ce573a3b652fcb62085fda7dd61aa
Alvaro <alvarinchu@gmail.com>
2021-01-30 17:14:32 +0100

    commit 1

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const { static } = require('express');
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
 app.use(express.static('./public'));
 
-app.get('/data/:id/:name?', function (req, res) {
-    console.log(req.params);
-    console.log(req.query);
-    res.send(`Hello World ${req.url}`);
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+app.set('view engine', 'hbs');
+
+app.get('/', function (req, res) {
+    res.render('home', {
+        nombre: 'Alvaro',
+        anio: new Date().getFullYear(),
+    });
+    //res.send(`Hello World ${req.url}`);
 });
 
 app.post('/data', (req, res) => {
