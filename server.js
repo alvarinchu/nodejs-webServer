7a0cc860a0dcafd9f86245968e28eaec0e4da9ec
Alvaro <alvarinchu@gmail.com>
2021-01-30 17:44:56 +0100

    Versión final

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+const helpers = require('./hbs/helpers');
 
 app.use(express.static('./public'));
 
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 app.set('view engine', 'hbs');
 
+// helpers
+
 app.get('/', function (req, res) {
-    res.render('home', {
-        nombre: 'Alvaro',
-        anio: new Date().getFullYear(),
-    });
+    res.render('home');
     //res.send(`Hello World ${req.url}`);
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', {
-        anio: new Date().getFullYear(),
-    });
+    res.render('about');
     //res.send(`Hello World ${req.url}`);
 });
 
